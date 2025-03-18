6ae04a84ec8e1b52b2638537f55ac589ea13af32
IchCodes <hichame.sraidi@gmail.com>
2025-03-18 17:50:23 +0100

    ajout header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Routes from "./routes/AppRoutes";
+import Header from "./components/Header";
 
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
+                <Header />
                 <Routes />
             </AuthProvider>
         </BrowserRouter>
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+
+const Header = () => {
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  const handleLogout = () => {
+    logout(navigate);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          DonaGo
+        </Typography>
+        {user ? (
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <Button color="inherit" onClick={handleLogin}>
+            Login
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
